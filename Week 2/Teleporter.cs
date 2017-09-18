9d1b8fe9b0d462c4a4abb236b96960adfb920bf8
Sean Han Tani <seagaia@f-m.fm>
2017-09-18 13:41:01 -0500

    Add Week 2 Teleporter

---
+++ Week 2/Teleporter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+	public string destinationName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// OnTriggerStay2D, OnTriggerExit2D
+	void OnTriggerEnter2D(Collider2D playerCollider) {
+		GameObject destinationTeleporter = GameObject.Find (destinationName);
+
+		Vector3 newPlayerPosition = new Vector3 ();
+		newPlayerPosition = destinationTeleporter.transform.position;
+
+		BoxCollider2D destinationTeleportCollider = destinationTeleporter.GetComponent <BoxCollider2D> ();
+
+		newPlayerPosition.x += destinationTeleportCollider.bounds.extents.x;
+		newPlayerPosition.x += playerCollider.bounds.extents.x;
+		newPlayerPosition.x += 1f;
+		 
+		playerCollider.transform.position = newPlayerPosition;
+	}
+
+	void OnTriggerStay2D(Collider2D playerCollider) {
+		
+	}
+
+}
