fdb7d9aa0dbf166cade599906ef67d3ba89a8e5b
Sean Han Tani <seagaia@f-m.fm>
2017-09-18 15:59:09 -0500

    aaaa

---
+++ Week 2/WalkAnimationTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkAnimationTest : MonoBehaviour {
+
+
+	// This script will change the state of the attached game object's animation controller
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// You can add other states inside the Animator window.
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			animator.Play("Walk");
+		} else {
+			animator.Play ("Idle");	
+		}
+
+	}
+}
