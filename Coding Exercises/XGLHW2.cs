addcf0e9046534e8ba71d669756b3be2ec78483c
Sean Han Tani <seagaia@f-m.fm>
2017-10-29 20:59:16 -0500

    Added exercise 2.

---
+++ Coding Exercises/XGLHW2.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// XGL Homework 2. This will test your skills with string manipulation functions.
+// You'll need to write if-else statements as well.
+
+public class XGLHW2 : MonoBehaviour {
+
+	/*
+	 * String Equality. Getting a character. Turning a character to a string.
+	 * Concatenation. IndexOf(). Length. Substring.
+	 * */
+
+	// Return true if the input string is equal to "cool".
+	// "cool" -> true
+	// "not cool" -> false
+	// "COOL" -> false
+	static public bool _01_isItCool(string input) {
+		return false;
+	}
+
+	// Given two strings, a and b, 
+	// if the pattern ID is 1, return the strings combined in the form a + b,
+	// if the pattern ID is 2, return the strings combined in the form b + a.
+	// otherwise, return "invalid pattern".
+	// You'll need to use string concatenation - combining strings - which requires using the "+" symbol like you would
+	// for adding two numbers.
+
+	// apple,banana,1 -> "applebanana"
+	// apple,banana,2 -> "bananaapple"
+	// apple,banana,3 -> "invalid pattern"
+	static public string _02_stitcher(string a, string b, int patternID) {
+		return "invalid pattern";
+	}
+
+
+	/* Given an input string, return the input string but with its first character
+	 Repeated before and after the original string.
+	 horse -> hhorseh
+	 banana -> bbananab */
+	static public string _03_front1(string input) {
+		return "";
+	}
+
+	/* Return true if the input string starts with the letter 'a' or 'A'.
+	 You can get a character in a string using array notation: input[i] where 'i' is the index of the character you want
+	 Note that to convert a single character from a string, you need to use .ToString() , e.g.:
+				input[4].ToString() ...
+
+	 "apple" -> true
+	 "Arkansas" -> true
+	 "banana" -> false */
+	static public bool _04_startsWithA(string input) {
+		return false;
+	}
+
+
+	/*Given a string, return a new string where "not " has been added to the front. 
+	However, if the string already begins with "not", return the string unchanged. 
+	Hint: use IndexOf to find if a string contains some other string.
+	Hint: IndexOf returns an integer, which represents the index in the string in which the search string was found.
+
+	("candy") → "not candy"
+	("x") → "not x"
+	("not bad") → "not bad" */
+	static public string _05_notString(string input) {	
+		return "";
+	}
+
+
+	// return 'length' # of characters from the input string, starting at index 'startIndex', but
+	// only if the length of 'input' is greater than or equal to 'length'. If input is less than length,
+	// return the empty string "".
+
+	// Note you can use .Length on a string, to get the length of a string.
+	// You can use .Substring(i,j) to get a 'substring' of length j starting at index i. Note that 
+	// strings index their characters starting at 0.
+
+	// horse,6,3 -> ""
+	// horse, 3, 1 -> "ors"
+	// 
+	static public string _06_redundantSubstring(string input, int length, int startIndex) {
+		return "";
+	}
+
+	// Return the string, but with the parts before and after a certain character flipped.
+	// index will always be less than input.Length.
+	// "apple",2 -> "le"+"p"+"ap" (or "lepap")
+	// "boy",0 -> "oy"+""+"b" (or "oyb")
+	// "boy",1 -> "y"+"o"+"b" (or "yob")
+	// "boy",2 -> ""+"y"+"bo" (or "ybo")
+	static public string _07_trickyString(string input, int index) {
+		return "";
+	}
+
+
+
+	// This function does nothing, but acts as a reference for correct syntax.
+	void exampleSyntax() {
+		// Basic math operators. You can also group things by parentheses.
+		int a = 0;
+		int b = 0;
+		a = a + b;
+		a = a - b;
+		a = a * b;
+		a = a / b;
+		a = (a + 1) * b;
+		a = (a + (b - 4)) * (b - (a / 2));
+
+		// Increment/decrement operators. Increases or decreases a variable by 1.
+		a ++;
+		a--;
+		a += 1;
+		a -= 1;
+
+		// Shorthand multiplication (equivalent to a = a * 2, and a = a / 2);
+		a *= 2;
+		a /= 2;
+
+		// if-else statements. 
+		bool c = false;
+		bool d = false;
+
+		if (c) {
+			// Do something because c was true.
+		}
+
+		if (c) {
+			// Do s.t. b/c c was true.
+		} else {
+			// Otherwise, do some default action.
+		}
+
+		if (c) {
+			// Do s.t. b/c c was true.
+		} else if (d) {
+			// otherwise, do s.t. b/c d was true.
+		} else {
+			// Otherwise, do some default action.
+		}
+
+
+		string str = "apple";
+		str = "apple" + "banana"; // str is now "applebanana"
+		int i = 1;
+		string ith_letter = str[i].ToString ();
+		ith_letter = ith_letter + ith_letter;
+
+		// .Substring() gets part of an existing string, or a 'substring'.
+		// The first parameter is what character of the string the
+		// substring should start from, the second parmater is how long the substring should be
+		// In this case, we are getting 5 characters of the string 'str', starting at index 0.
+		// So we are getting "apple" out of "applebanana".
+		string partOfString = str.Substring (0, 5); // partOfString is now "apple".
+
+		partOfString = partOfString + "1";
+	}
+}
--- Coding Exercises/XGLHWTESTER.cs
+++ Coding Exercises/XGLHWTESTER.cs
@@ -13,44 +13,88 @@ public class XGLHWTESTER : MonoBehaviour {
 
 	[Tooltip("Set to true if the grading script should grade this exercise.")]
 	public bool gradeExercise1 = true;
+	public bool gradeExercise2 = true;
+
 	void Start () {
 
-		VERIFY_INT (1,0,XGLHW1._01_simplest_sum (0,0),"0,0",1);
-		VERIFY_INT (1,0,XGLHW1._01_simplest_sum (-1,1),"-1,1",1);
-		VERIFY_INT (1,14,XGLHW1._01_simplest_sum (7,7),"7,7",1);
-		VERIFY_INT (1,-1,XGLHW1._01_simplest_sum (2,-3),"2,-3",1);
+
+		if (gradeExercise1) {
+			VERIFY_INT (1, 0, XGLHW1._01_simplest_sum (0, 0), "0,0", 1);
+			VERIFY_INT (1, 0, XGLHW1._01_simplest_sum (-1, 1), "-1,1", 1);
+			VERIFY_INT (1, 14, XGLHW1._01_simplest_sum (7, 7), "7,7", 1);
+			VERIFY_INT (1, -1, XGLHW1._01_simplest_sum (2, -3), "2,-3", 1);
 
 
-		VERIFY_INT (2,0,XGLHW1._02_simpler_sum (0,0),"0,0",1);
-		VERIFY_INT (2,8,XGLHW1._02_simpler_sum (2,3),"2,3",1);
-		VERIFY_INT (2,3,XGLHW1._02_simpler_sum (1,1),"1,1",1);
+			VERIFY_INT (2, 0, XGLHW1._02_simpler_sum (0, 0), "0,0", 1);
+			VERIFY_INT (2, 8, XGLHW1._02_simpler_sum (2, 3), "2,3", 1);
+			VERIFY_INT (2, 3, XGLHW1._02_simpler_sum (1, 1), "1,1", 1);
 
-		VERIFY_BOOL (3,false,XGLHW1._03_hotDay (10),"10");
-		VERIFY_BOOL (3,false,XGLHW1._03_hotDay (79),"79");
-		VERIFY_BOOL (3,false,XGLHW1._03_hotDay (80),"80");
-		VERIFY_BOOL (3,true,XGLHW1._03_hotDay (105),"105");
+			VERIFY_BOOL (3, false, XGLHW1._03_hotDay (10), "10");
+			VERIFY_BOOL (3, false, XGLHW1._03_hotDay (79), "79");
+			VERIFY_BOOL (3, false, XGLHW1._03_hotDay (80), "80");
+			VERIFY_BOOL (3, true, XGLHW1._03_hotDay (105), "105");
 
-		VERIFY_INT (4,1,XGLHW1._04_simpleSum (0,1),"0,1");
-		VERIFY_INT (4,3,XGLHW1._04_simpleSum (1,2),"1,2");
-		VERIFY_INT (4,20,XGLHW1._04_simpleSum (5,5),"5,5");
-		VERIFY_INT (4,72,XGLHW1._04_simpleSum (34,2),"34,2");
+			VERIFY_INT (4, 1, XGLHW1._04_simpleSum (0, 1), "0,1");
+			VERIFY_INT (4, 3, XGLHW1._04_simpleSum (1, 2), "1,2");
+			VERIFY_INT (4, 20, XGLHW1._04_simpleSum (5, 5), "5,5");
+			VERIFY_INT (4, 72, XGLHW1._04_simpleSum (34, 2), "34,2");
 
-		VERIFY_INT (5,6,XGLHW1._05_kindaSimpleSum (true,1,2),"true,1,2");
-		VERIFY_INT (5,3,XGLHW1._05_kindaSimpleSum (false,1,2),"false,1,2");
-		VERIFY_INT (5,88,XGLHW1._05_kindaSimpleSum (true,12,32),"true,12,32");
+			VERIFY_INT (5, 6, XGLHW1._05_kindaSimpleSum (true, 1, 2), "true,1,2");
+			VERIFY_INT (5, 3, XGLHW1._05_kindaSimpleSum (false, 1, 2), "false,1,2");
+			VERIFY_INT (5, 88, XGLHW1._05_kindaSimpleSum (true, 12, 32), "true,12,32");
 
-		VERIFY_INT (6,10,XGLHW1._06_absoluteValue (-10),"-10");
-		VERIFY_INT (6,1,XGLHW1._06_absoluteValue (1),"1");
-		VERIFY_INT (6,1424,XGLHW1._06_absoluteValue (1424),"1424");
-		VERIFY_INT (6,64,XGLHW1._06_absoluteValue (-64),"-64");
+			VERIFY_INT (6, 10, XGLHW1._06_absoluteValue (-10), "-10");
+			VERIFY_INT (6, 1, XGLHW1._06_absoluteValue (1), "1");
+			VERIFY_INT (6, 1424, XGLHW1._06_absoluteValue (1424), "1424");
+			VERIFY_INT (6, 64, XGLHW1._06_absoluteValue (-64), "-64");
 
-		VERIFY_BOOL (7,false,XGLHW1._07_goodParty (5,4),"5,4");
-		VERIFY_BOOL (7,false,XGLHW1._07_goodParty (5,1),"5,1");
-		VERIFY_BOOL (7,false,XGLHW1._07_goodParty (10,1),"10,1");
-		VERIFY_BOOL (7,true,XGLHW1._07_goodParty (10,5),"10,5");
-		VERIFY_BOOL (7,true,XGLHW1._07_goodParty (10,6),"10,6");
+			VERIFY_BOOL (7, false, XGLHW1._07_goodParty (5, 4), "5,4");
+			VERIFY_BOOL (7, false, XGLHW1._07_goodParty (5, 1), "5,1");
+			VERIFY_BOOL (7, false, XGLHW1._07_goodParty (10, 1), "10,1");
+			VERIFY_BOOL (7, true, XGLHW1._07_goodParty (10, 5), "10,5");
+			VERIFY_BOOL (7, true, XGLHW1._07_goodParty (10, 6), "10,6");
 
-		printGrade (1);
+			printGrade (1);
+		}
+		score = maxScore = 0;
+
+		if (gradeExercise2) {
+
+
+			VERIFY_BOOL (1,true,XGLHW2._01_isItCool ("cool"),"cool");
+			VERIFY_BOOL (1,false,XGLHW2._01_isItCool ("horse"),"horse");
+
+			VERIFY_STRING (2,"ab",XGLHW2._02_stitcher ("a","b",1),"a,b,1");
+			VERIFY_STRING (2, "ba", XGLHW2._02_stitcher ("a", "b", 2), "a,b,2");
+			VERIFY_STRING (2, "eeebafea", XGLHW2._02_stitcher ("afea", "eeeb", 2), "afea,eeeb,2");
+			VERIFY_STRING (2, "invalid pattern", XGLHW2._02_stitcher ("a", "b", 3), "a,b,3");
+
+			VERIFY_STRING (3, "aaa", XGLHW2._03_front1 ("a"), "a");
+			VERIFY_STRING (3, "bbobb", XGLHW2._03_front1 ("bob"), "bob");
+			VERIFY_STRING (3, "ccurryc", XGLHW2._03_front1 ("curry"), "curry");
+
+			VERIFY_BOOL(4,false,XGLHW2._04_startsWithA ("banana"),"banana");
+			VERIFY_BOOL(4,true,XGLHW2._04_startsWithA ("apple"),"apple");
+			VERIFY_BOOL(4,true,XGLHW2._04_startsWithA ("Arby's"),"Arby's");
+
+			VERIFY_STRING (5,"notapple",XGLHW2._05_notString ("apple"),"apple");
+			VERIFY_STRING (5,"not horse",XGLHW2._05_notString ("not horse"),"not horse");
+			VERIFY_STRING (5,"notNOT apple",XGLHW2._05_notString ("NOT apple"),"NOT apple");
+
+			VERIFY_STRING (6,"",XGLHW2._06_redundantSubstring ("horse",6,1),"horse,6,1");
+			VERIFY_STRING (6,"ors",XGLHW2._06_redundantSubstring ("horse",3,1),"horse,3,1");
+			VERIFY_STRING (6,"mcdon",XGLHW2._06_redundantSubstring ("mcdonald",5,0),"mcdonalds,5,0");
+
+			VERIFY_STRING (7,"oyb",XGLHW2._07_trickyString ("boy",0),"boy,0");
+			VERIFY_STRING (7,"yob",XGLHW2._07_trickyString ("boy",1),"boy,1");
+			VERIFY_STRING (7,"ybo",XGLHW2._07_trickyString ("boy",2),"boy,2");
+			VERIFY_STRING (7,"abanan",XGLHW2._07_trickyString ("banana",5),"banana,5");
+			VERIFY_STRING (7,"ananab",XGLHW2._07_trickyString ("banana",0),"banana,0");
+			VERIFY_STRING (7,"herwWERWrraaaeru",XGLHW2._07_trickyString ("aaeruaherwWERWrr",5),"aaeruaherwWERWrr,5");
+
+			printGrade (2);
+		}
+
 	}
 	
 	bool VERIFY_INT(int questionID, int answer, int studentAnswer, string testInput,float value=1) {
@@ -95,6 +139,28 @@ public class XGLHWTESTER : MonoBehaviour {
 		}
 		return true;
 	}
+
+	bool VERIFY_STRING(int questionID, string answer, string studentAnswer, string testInput,float value=1) {
+		maxScore += value;
+		string outLine = "";
+		outLine = "[" + questionID.ToString () + "]";
+		if (studentAnswer != answer) {
+			outLine += " [WRONG  ]";
+			outLine += " (" + testInput + ") -> " + studentAnswer + "[EXPECTED: "+answer +"]";
+
+			outLine += string.Format ("[0/{0} points]", value);
+			print (outLine);
+			return false;
+		} else {
+			outLine += " [CORRECT]";
+			outLine += " (" + testInput + ") -> " + studentAnswer;
+			score += value;
+			outLine += string.Format ("[{0}/{1} points]", value, value);
+			if (!hideCorrect) print (outLine);
+		}
+		return true;
+	}
+
 
 	void printGrade(int HWID) {
 		print (string.Format("[EXERCISE {0} SCORE]: {1:P} ({2}/{3}) points",HWID,score/maxScore,score,maxScore)); 
