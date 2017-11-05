08bc70d9c52c3c0e2e0cf8193b9e6923a187bebc
Sean Han Tani <seagaia@f-m.fm>
2017-11-05 16:09:37 -0600

    Add Homework 3.

---
+++ Coding Exercises/XGLHW3.cs
@@ -0,0 +1,252 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// XGL Homework 3. This will help you get used to for loops and using arrays.
+
+public class XGLHW3 : MonoBehaviour {
+
+
+
+	// Fix this for-loop so that it adds four to the input!
+	// Remember, a for loop has three parts in the "( )":
+	// 1. the iterator variable declaration(int i = 0), which keeps track of how many times the loop has run
+	// 2. the conditional expression (i < 2), which if true, allows the for loop to run again,
+	// 3. and the increment expression (i++), or how much the iterator changes every time the loop runs.
+
+	// Hint: you only need to change one number here.
+
+	// Examples:
+	// 2 -> 6
+	// 5 -> 9
+	// -4 -> 0
+	static public int loop_1_add_four(int input) {
+
+		int output = input;
+		for (int i = 0; i < 2; i++) {
+			output++;
+		}
+		return output;
+	}
+
+
+	// Return an array of size three, with the three elements a, b, and c.
+	// It's array time! arrays are fixed-sized data structures that hold a single type of value. 
+	// Creating an array can be done in two ways:
+	// TYPE[] ARRAYNAME = new TYPE[]{VALUE1,VALUE2,...}
+	//		e.g.: int[] integerArray = new int[]{1,4,5,3};
+	// TYPE[] ARRAYNAME = new TYPE[ARRAYSIZE];
+	//		e.g.: string[] stringArray = new string[3];
+
+	// Examples
+	// 1,4,5 -> [1,4,5]
+	// 0,0,1 -> [0,0,1]
+	static public int[] array_creation(int a, int b, int c) {
+		return new int[1];
+	}
+
+
+	// return the first element of the array!
+	// You can access an element of an array using the "[]" syntax:
+	// a[3] = return the 4th element from a. a[2] = the 3rd element, etc.
+	// Note, arrays index from zero. So the "4th" element actually has an index of 3. The 1st element has an index of 0.
+	// Note: a will always have at least one element
+	// Examples:
+	// [1,2,3] -> 1
+	// [3,4,5] -> 3
+	// [4,1,4,2,2,2,2,2,2,3,4,1,1,4,1,4,1,1] -> 4
+	static public int array_getTheFirstElement(int[] a) {
+		return -1;
+	}
+
+	// return the i-th element of the array!
+	//Note: i will always be > 0 and < a.Length
+
+	// Examples:
+	// [1,2,3],2 -> 3
+	// [3,4,5],0 -> 3
+	// [4,1,4,2,2,2,2,2,2,3,4,1,1,4,1,4,1,1],2 -> 4
+	static public int array_getTheIthElement(int[] a, int i) {
+		return -1;
+	}
+
+	// Return a single string made up of the concatenation of the first "num" strings in input. You'll need to make a loop!
+	// Hint: You'll somehow need to incorporate "num" into the three parts of your for loop's initialization.
+	// Note: You can assume 'num' will always be > 0 and <=  input.Length , and that input always has at least one element
+
+	// Examples:
+	// ["a","b","c"],3 -> "abc"
+	// ["a","b","c"],1 -> "a"
+	static public string loop_bigString(string[] input, int num) {
+		return "";
+	}
+
+	// Use .Length to return the length of the input array. 
+	// E.g. if "input" = [0,2,3,4], then "input.Length" would give you 4, because there are 4 elements in "input".
+	// [0] -> 1
+	// [4,3,5,1,2] -> 5
+	static public int array_length(int[] input ){ 
+		return -1;
+	}
+
+	// return the sum of every element in the array.
+	// Hint: you'll need to use input.Length somehow.
+	// examples
+	// [1,1,1] -> 3
+	// [3] -> 3
+	// [5,4,3,2,1] -> 15
+	static public int array_sum(int[] input) {
+		return -1;
+	}
+
+
+	// return the sum of every element in the array with an even index (0,2,4, etc.)
+	// You will need to use the modulo/remainder (%) operator, which works like the following:
+	//  a % b = the remainder of a divided by b.
+	//	4 % 2 = 0
+	//  1 % 2 = 1
+	//	3 % 2 = 1
+	//  5 % 3 = 2
+
+	// examples
+	// [1,1,1] -> 2
+	// [3] -> 3
+	// [5,4,3,2,1] -> 9
+	static public int array_EvenSum(int[] input) {
+		return -1;
+	}
+
+
+	// Change the second element of the array to the first element, and return the array.
+	// [1,2] -> [1,1]
+	// [2,0,1] -> [2,2,1]
+	static public int[] array_3_changer(int[] input) {
+		return input;
+	}
+
+	// Change the a-th element of the array to the b-th element of the array, only if the a-th element is > than the b-th element.
+	// Return the possibly modified array.
+	// [1,2],0,1 -> [1,2]
+	// [2,0,1],0,2 -> [1,0,1]
+	static public int[] array_4_changer2(int[] input, int a, int b) {
+		return input;
+	}
+
+	// A Vector3 is an object. Objects are just special types of variables that can have many properties associated with them.
+	//
+	// A Vector 3 contains 3 properties (or values), x, y, and z. In the context of Vector3, these values are also called 'components' to 
+	//		refer to the components of a vector in 3D space.
+	// Return the x component of the vector if "getX" is true, otherwise return the y component.
+	// To access a component of a Vector named 'v':
+	//		v.x 
+	//		v.y
+	// Vector3 w/ components {1,2,3},false -> 2
+	// Vector3 w/ components {1,2,3},true -> 1
+	static public float vector_getComponent(Vector3 v, bool getX) {
+		return -1;
+	}
+
+	// Is the position vector of this transform's x property greater than zero?
+	// Note: A transform represents the position, rotation and scale of a gameObject in Unity.
+	// To access properties of a transform (or any other object), use the dot operator, '.', just like you used to access a component of a vector3.
+	// E.g. : transform.position transform.quaternion, transform.eulerAngles
+	// transform with position {4,3,-1} -> true
+	// transform with position {-2,3,1} -> false
+	static public bool vector_isXPositive(Transform transform) {
+		return false;	
+	}
+
+	// Given an array of game objects, does at least one of them have an x-position that is > 0?
+	// You loop through arrays of objects just like you do arrays of ints and strings.
+	// A GO/gameobject always has a transform property.
+	// [GO w/ pos {1,2,3}, GO w/ pos {0,2,3}] -> true
+	// [GO w/ pos {-1,2,3}, GO w/ pos {0,2,3}] -> false
+	static public bool vector_isOnePositive(GameObject[] objects) {
+		return false;
+	}
+
+	// This function does nothing, but acts as a reference for correct syntax.
+	void exampleSyntax() {
+
+		int[] initiallyEmptyIntArrayOfSizeFive = new int[5];
+		int[] intArrayWithInitialElements = new int[]{ 5, 4, 1 };
+
+		int[] testArray = new int[]{ 1, 2, 3, 4, 5 };
+		testArray [1] = 2; // Change an element of an array. In this case, the 2nd element of the array (or element with index 1) will change.
+
+
+		// Run this code 5 times.
+		for (int i = 0; i < 5; i++) {
+			// Use a variable as the index into an array.
+			initiallyEmptyIntArrayOfSizeFive [i] = 3;
+		}
+
+		int someNumber = 6;
+		for (int i = 0; i < someNumber; i++) {
+			// Do something someNumber times.
+		}
+
+		for (int i = 0; i < intArrayWithInitialElements.Length; i++){
+			// Do something as many times as there are elements in intArrayWithInitialElements.
+		}
+
+		// Basic math operators. You can also group things by parentheses.
+		int a = 0;
+		int b = 0;
+		a = a + b;
+		a = a - b;
+		a = a * b;
+		a = a / b;
+		a = (a + 1) * b;
+		a = (a + (b - 4)) * (b - (a / 2));
+
+		// Increment/decrement operators. Increases or decreases a variable by 1.
+		a ++;
+		a--;
+		a += 1;
+		a -= 1;
+
+		// Shorthand multiplication (equivalent to a = a * 2, and a = a / 2);
+		a *= 2;
+		a /= 2;
+
+		// if-else statements. 
+		bool c = false;
+		bool d = false;
+
+		if (c) {
+			// Do something because c was true.
+		}
+
+		if (c) {
+			// Do s.t. b/c c was true.
+		} else {
+			// Otherwise, do some default action.
+		}
+
+		if (c) {
+			// Do s.t. b/c c was true.
+		} else if (d) {
+			// otherwise, do s.t. b/c d was true.
+		} else {
+			// Otherwise, do some default action.
+		}
+
+
+		string str = "apple";
+		str = "apple" + "banana"; // str is now "applebanana"
+		int ai = 1;
+		string ith_letter = str[ai].ToString ();
+		ith_letter = ith_letter + ith_letter;
+
+		// .Substring() gets part of an existing string, or a 'substring'.
+		// The first parameter is what character of the string the
+		// substring should start from, the second parmater is how long the substring should be
+		// In this case, we are getting 5 characters of the string 'str', starting at index 0.
+		// So we are getting "apple" out of "applebanana".
+		string partOfString = str.Substring (0, 5); // partOfString is now "apple".
+
+		partOfString = partOfString + "1";
+	}
+}
--- Coding Exercises/XGLHWTESTER.cs
+++ Coding Exercises/XGLHWTESTER.cs
@@ -14,6 +14,7 @@ public class XGLHWTESTER : MonoBehaviour {
 	[Tooltip("Set to true if the grading script should grade this exercise.")]
 	public bool gradeExercise1 = true;
 	public bool gradeExercise2 = true;
+	public bool gradeExercise3 = true;
 
 	void Start () {
 
@@ -94,6 +95,62 @@ public class XGLHWTESTER : MonoBehaviour {
 
 			printGrade (2);
 		}
+		score = maxScore = 0;
+
+		if (gradeExercise3) {
+
+			VERIFY_INT (1, 12, XGLHW3.loop_1_add_four (8), "8");
+
+			VERIFY_ARRAY_INT (2, new int[]{3,5,1}, XGLHW3.array_creation (3,5,1), "3,5,1");
+			VERIFY_INT (3, 1, XGLHW3.array_getTheFirstElement (new int[]{ 1, 2, 3 }), "[1,2,3]");
+
+			VERIFY_INT (4, 10, XGLHW3.array_getTheIthElement (new int[]{ 1, 10, 3 },1), "[1,10,3],1");
+			VERIFY_INT (4, 1, XGLHW3.array_getTheIthElement (new int[]{ 1, 10, 3 },0), "[1,10,3],0");
+			VERIFY_INT (4, 3, XGLHW3.array_getTheIthElement (new int[]{ 1, 10, 3 },2), "[1,10,3],2");
+
+			VERIFY_STRING (5, "cde", XGLHW3.loop_bigString (new string[]{ "c", "d", "e", "f" }, 3), "[c,d,e,f],3");
+			VERIFY_STRING (5,"c",XGLHW3.loop_bigString (new string[]{"c","d","e","f"},1),"[c,d,e,f],1");
+			VERIFY_STRING (5,"cdef",XGLHW3.loop_bigString (new string[]{"c","d","e","f"},4),"[c,d,e,f],4");
+			VERIFY_STRING (5,"iLoveGames",XGLHW3.loop_bigString (new string[]{"i","Love","Games"},3),"[i,Love,Games],3");
+
+			VERIFY_INT (6, 5, XGLHW3.array_length (new int[5]), "[0,0,0,0,0]");
+			VERIFY_INT (6, 3, XGLHW3.array_length (new int[]{1,4,3}), "[1,4,3]");
+
+			VERIFY_INT (7, 3, XGLHW3.array_length (new int[]{1,4,3}), "[1,4,3]");
+			VERIFY_INT (7, 5, XGLHW3.array_length (new int[]{1,1,1,1,1}), "[1,1,1,1,1]");
+
+			VERIFY_INT (8, 4, XGLHW3.array_EvenSum (new int[]{1,4,3}), "[1,4,3]");
+			VERIFY_INT (8, 3, XGLHW3.array_EvenSum (new int[]{1,1,1,1,1}), "[1,1,1,1,1]");
+
+			VERIFY_ARRAY_INT (9, new int[]{ 1, 1, 3 }, XGLHW3.array_3_changer (new int[]{ 1, 2, 3 }), "[1,2,3]");
+
+			VERIFY_ARRAY_INT (10, new int[]{ 1, 1, 3 }, XGLHW3.array_4_changer2 (new int[]{ 2,1, 3 },0,1), "[2,1,3],0,1");
+			VERIFY_ARRAY_INT (10, new int[]{ 4,5,6 }, XGLHW3.array_4_changer2 (new int[]{ 4,5,6 },1,2), "[4,5,6],1,2");
+
+			VERIFY_INT (11, 4, (int) XGLHW3.vector_getComponent (new Vector3 (4, 2, 3), true), "{4,2,3},true");
+			VERIFY_INT (11, 2, (int) XGLHW3.vector_getComponent (new Vector3 (4, 2, 3), false), "{4,2,3},false");
+
+			GameObject g1 = new GameObject ();
+			Transform t1 = g1.transform; t1.position = new Vector3 (1, 4, 3);
+			VERIFY_BOOL (12, true, XGLHW3.vector_isXPositive (t1), "Transform w/ position {1,4,3}");
+			t1.position = new Vector3 (-1, 4, 3);
+			VERIFY_BOOL (12, false, XGLHW3.vector_isXPositive (t1), "Transform w/ position {-1,4,3}");
+
+			GameObject g2 = new GameObject ();
+			GameObject[] ga = new GameObject[]{ g1, g2 };
+
+			g1.transform.position = new Vector3 (1, 2, 3);
+			g2.transform.position = new Vector3 (-1, 2, 3);
+
+			VERIFY_BOOL (13, true, XGLHW3.vector_isOnePositive (ga), "[go w/ pos {1,2,3},go w/ pos {-1,2,3}]");
+
+
+			g1.transform.position = new Vector3 (-1, 2, 3);
+			VERIFY_BOOL (13, false, XGLHW3.vector_isOnePositive (ga), "[go w/ pos {-1,2,3},go w/ pos {-1,2,3}]");
+
+
+			printGrade (3);
+		}
 
 	}
 	
@@ -161,6 +218,55 @@ public class XGLHWTESTER : MonoBehaviour {
 		return true;
 	}
 
+	bool VERIFY_ARRAY_INT(int questionID, int[] answer, int[] studentAnswer, string testInput, float value=1) {
+
+		maxScore += value;
+		string outLine = "";
+		outLine = "[" + questionID.ToString () + "]";
+		string studentArrayAsStr = "[";
+		bool matches = true;
+		if (answer.Length == studentAnswer.Length) {
+			for (int i =0; i < answer.Length; i++) {
+				if (answer[i] != studentAnswer[i]) {
+					matches = false;
+				}
+			}
+		} else {
+			matches = false;
+		}
+
+		for (int i = 0; i < studentAnswer.Length; i++) {
+			studentArrayAsStr += studentAnswer [i].ToString ();
+			if (i != studentAnswer.Length-1) studentArrayAsStr += ",";
+		}
+		studentArrayAsStr += "]";
+
+
+		string answerAsStr = "[";
+		for (int i =0; i < answer.Length; i++) {
+			answerAsStr += answer [i].ToString ();
+			if (i != answer.Length-1) answerAsStr += ",";
+		}
+		answerAsStr += "]";
+
+		if (matches) {
+			outLine += " [CORRECT]";
+			outLine += " (" + testInput + ") -> " + studentArrayAsStr;
+			score += value;
+			outLine += string.Format ("[{0}/{1} points]", value, value);
+			if (!hideCorrect)
+				print (outLine);
+		} else {
+			outLine += " [WRONG]";
+			outLine += " (" + testInput + ") -> " + studentArrayAsStr + "[EXPECTED: "+answerAsStr +"]";
+
+			outLine += string.Format ("[0/{0} points]", value);
+			print (outLine);
+			return false;
+		}
+		return true;
+
+	}
 
 	void printGrade(int HWID) {
 		print (string.Format("[EXERCISE {0} SCORE]: {1:P} ({2}/{3}) points",HWID,score/maxScore,score,maxScore)); 
