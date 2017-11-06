c458279edcdd9c379b96b192c1a9d343d6f80687
Sean Han Tani <seagaia@f-m.fm>
2017-11-06 14:03:08 -0600

    Add InputManager

---
+++ Week 9/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// You can attach this to a singleton instance,
+// Or add it to a prefab and include that prefab in every scene.
+public class InputManager : MonoBehaviour {
+
+	public bool rightHeld = false;
+	public bool leftHeld = false;
+	public bool rightJustPressed = false;
+	public bool leftJustPressed = false;
+
+	KeyCode kc_right = KeyCode.D;
+	KeyCode kc_left = KeyCode.A;
+	void Update () {
+
+		rightHeld = Input.GetKey (kc_right);
+		leftHeld = Input.GetKey (kc_left);
+		rightJustPressed = Input.GetKeyDown (kc_right);
+		leftJustPressed = Input.GetKeyDown (kc_left);
+
+	}
+}
