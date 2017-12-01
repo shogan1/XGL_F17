deae557a3bb82ff5346fbbe56135b5f4c90dede3
Sean Han Tani <seagaia@f-m.fm>
2017-12-01 12:59:17 -0600

    audio stuff

---
+++ Audio Stuff/SceneChangeTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// When overlapping the player, changes the scene.
+// Challenge: How would you get this script to warp you to a particular point 
+//	in the next scene? 
+//
+// 				Hint: You will need to create a GameObject in the destination scene, and this Game OBject
+//					  will represent the position your SceneChangeTrigger will send the player to
+// 				Hint: You will need to store the name of that destination GameObject in a way that lets you
+// 					  use the name between two scenes (Hint: static variables let you store stuff between scenes)
+//				Hint: You need to add a script to the player that will check if the player should warp to a destination.
+//						(Where should this warping code go? Start() or Update()?)
+//			
+
+
+
+// You have to type this *outside of the class*, to get access to Scene functions in Unity.
+using UnityEngine.SceneManagement;
+
+
+public class SceneChangeTrigger : MonoBehaviour {
+
+	public string destinationScene;
+
+
+	void OnTriggerEnter(Collider c) {
+		if (c.CompareTag ("Player")) {
+			// Note: this won't work unless the destination scene has been added to the Build
+			// inside of Build Settings.
+			SceneManager.LoadScene (destinationScene);
+
+		}
+	}
+}
---
+++ Audio Stuff/SongChangeTrigger.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongChangeTrigger : MonoBehaviour {
+
+	BoxCollider bc;
+	public AudioSource audioSourceToFadeIn;
+	public AudioSource audioSourceToFadeOut;
+	public float transitionTime = 2f;
+	// Use this for initialization
+	void Start () {
+
+		if (audioSourceToFadeIn == null || audioSourceToFadeOut == null) {
+			Debug.LogWarning ("You need to connect audio sources to this script through the inspector!");
+			enabled = false;
+			return;
+		}
+		bc = GetComponent <BoxCollider> ();
+		if (bc == null) {
+			Debug.LogWarning ("You need to add a Box Collider to this script and set 'is trigger' to true!");
+			enabled = false;
+			return;
+			
+		}
+				bc.isTrigger = true;
+
+		GameObject go = GameObject.Find ("FPSController");
+		if (go != null) {
+			go.tag = "Player";
+		}
+	}
+
+	int mode = 0;
+	float t = 0;
+	
+	// Update is called once per frame
+	void Update () {
+			
+		if (mode == 0) {
+			
+		} else if (mode == 1) {
+			if (audioSourceToFadeIn.volume == 1) {
+				mode = 0;
+				t = 0;
+				return;
+			}
+			
+			t += Time.deltaTime;
+			audioSourceToFadeIn.volume = Mathf.Lerp (0, 1, t / transitionTime);
+			audioSourceToFadeOut.volume = Mathf.Lerp (1,0, t / transitionTime);
+
+			if (t >= transitionTime) {
+				t = 0;
+				mode = 0;
+			}
+
+		}
+	}
+
+	void OnTriggerEnter(Collider c) {
+		if (c.CompareTag ("Player") == true) {
+			mode = 1;
+		}
+	}
+}
