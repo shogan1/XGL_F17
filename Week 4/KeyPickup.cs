ccb578ecb8e7ac57449b83011fe71f09758032e0
Sean Han Tani <seagaia@f-m.fm>
2018-02-19 14:04:12 -0600

    Key stuff

--- Week 4/KeyPickup.cs
+++ Week 4/KeyPickup.cs
@@ -6,23 +6,18 @@ using UnityEngine;
 public class KeyPickup : MonoBehaviour {
 
 
-	// Change the key color and ID as needed.
-	public bool isARedKey = true;
 	// This ID corresponds to the keyState array of a LockedDoor script.
 	public int keyID = 0;
 	// Drag this from the inspector
 	public LockedDoor door;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	void OnTriggerEnter(Collider c) {
-		//print (1);
 		if (c.CompareTag ("Player")) {
-			door.redKeyState [keyID] = true;
+            
+            door.setKey(keyID);
 			gameObject.SetActive (false);
+
 		}
 	}
 }
--- Week 4/LockedDoor.cs
+++ Week 4/LockedDoor.cs
@@ -3,42 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LockedDoor : MonoBehaviour {
-	
 
 	// Keeps track of which keys you have.
 	// A KeyPickup script will edit this array.
-	public bool[] redKeyState;
-	// Edit these in the editor, set each entry/item/element/value/whatevr of the array to true if you need that 
-	// corresponding key.
-	public bool[] requiredRedKeys;
-
-	public static int score = 0;
-
+	bool[] keyState;
+    // set this to some number!
+    public int keysNeeded = 0;
 
 	// Use this for initialization
 	void Start () {
 
-
-		// This is how you initialize an array with entries in it.
-		int[] numbers = new int[]{ 1, 4, 6 };
-
-		// There's only up to 2 red keys associated with any one door.
-		// This is how you initialize an array with a certain number of items
-		redKeyState = new bool[requiredRedKeys.Length];
-
+        keyState = new bool[keysNeeded];
 
 		// Arrays are indexed from 0, meaning the first item in the array has an index of 0
-		//redKeyState [0] = false;
-		//redKeyState [1] = false;
+		//keyState [0] = false;
+		//keyState [1] = false;
 		// This line would give an error, because it's trying to get 3rd item
-		//redKeyState [2] = false;
+		//keyState [2] = false;
 
 		// Arrays can't be resized once created, if you need to resize an array, use a list instead
 
 
-
 	}
 	
+    public void setKey(int index) {
+        keyState[index] = true;
+    }
+
 	// Update is called once per frame
 	void Update () {
 
@@ -47,10 +38,10 @@ public class LockedDoor : MonoBehaviour {
 
 
 		// For loop has three parts: iterator initialization, condition, end of loop code
-		for (int i = 0; i < redKeyState.Length; i++) {
+		for (int i = 0; i < keyState.Length; i++) {
 			// ...However, if we need the i'th key and don't have it, prevent the 
 			// door from being unlocked.
-			if (requiredRedKeys [i] && !redKeyState [i]) {
+			if (!keyState [i]) {
 				unlockDoor = false;
 			}
 		}
