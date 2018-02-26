b4ec966feb03ef36a598426f56e29784fa152b92
Sean Han Tani <seagaia@f-m.fm>
2018-02-26 14:18:39 -0600

    updates

--- Week 5/ClickShoot.cs
+++ Week 5/ClickShoot.cs
@@ -3,55 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-// S17 XGL wk10
-// 
-
-// attach this to anything
+// attach this to anything, only once per scene. It will detect mouse clicks and apply forces to things as needed
 
 // Uses a raycast to turn an object red and also apply a force to it.
-public class ClickShoot : MonoBehaviour {
+public class ClickShoot : MonoBehaviour
+{
 
-	void Start () {
-		
-	}
-	
-	void FixedUpdate () {
+    void Start()
+    {
+    }
 
-		// Check if mouse was left-clicked
-		if (Input.GetMouseButtonDown (0)) {
-
-			// Create a ray with origin at the camera and going *through* the mous eposition
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;
-
-			// Ray should go for 100 units
-			// Layer mask means that only stuff on layer 0 will be hit.
-			if (Physics.Raycast (ray, out hit, 100f, 1 << 0)) {
-				
-				// If it hits something, we can access that game object through our RaycastHit:
-				// hit.collider.gameObject
-
-				// Change the color
-				MeshRenderer mr = hit.collider.gameObject.GetComponent <MeshRenderer> ();
-				if (mr != null) {
-					mr.material.color = Color.red;
-				}
-
-				// Only apply a force if the gameObject HAS a rigidbody
-				Rigidbody rb = hit.collider.gameObject.GetComponent <Rigidbody> ();
-				if (rb != null) {
-					rb.AddForce (600f, 600f, 200f);
-				}
-
-			}
+    void FixedUpdate()
+    {
 
 
-			// This is if you want only the object this script is attached to, to be interactable
-			if (Physics.Raycast (ray, out hit, 100f)) {
-				if (hit.collider.gameObject.name == gameObject.name) {
-					// Do something...
-				}
-			}
-		}
-	}
+        // Check if mouse was left-clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            // Create a ray with origin at the camera and going *through* the mouse position
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // Creating a ray via a position and direction
+          //  Ray ray2 = new Ray(transform.position,new Vector3(0,1,0));
+
+            // Ray should go for 100 units
+
+            // The string array can have other strings added to it so the layer mask you get from GetMask will
+            // let you allow the raycast to hit other layers
+            if (Physics.Raycast(ray, out hit, 100f,LayerMask.GetMask(new string[]{"Default","Someotherlayer"})))  
+            {
+                // You can also make a layermask that lets the raycast hit multiplelayers
+              //  int multiLayerMask = LayerMask.GetMask(new string[]{ "Default", "Player"});
+
+                // If it hits something, we can access that game object through our RaycastHit lik eso.
+                // hit.collider.gameObject;
+
+
+                // Change the color
+                MeshRenderer mr = hit.collider.gameObject.GetComponent<MeshRenderer>();
+                if (mr != null)
+                {
+                    mr.material.color = Color.red;
+                }
+
+                // Only apply a force if the gameObject HAS a rigidbody
+                Rigidbody rb = hit.collider.gameObject.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    // Applies a force in direction of where camera faces
+                    rb.AddForce(Camera.main.transform.forward*300f);
+                }
+
+            }
+
+
+            // This is if you want only the object this script is attached to, to be interactable
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                if (hit.collider.gameObject.name == gameObject.name)
+                {
+                    // Do something...
+                }
+            }
+        }
+    }
 }
