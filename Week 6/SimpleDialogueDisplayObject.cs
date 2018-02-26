3525e595e5d1e96fbb0691e1bea5234f74fc66c3
Sean Han Tani <seagaia@f-m.fm>
2018-02-26 15:45:10 -0600

    update dialogue stuff

--- Week 6/SimpleDialogueDisplayObject.cs
+++ Week 6/SimpleDialogueDisplayObject.cs
@@ -3,39 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+// Attached to some trigger - the "textToShow" will appear on the screen when you overlap this trigger
+// You need to have a TextMeshPro gameobject in your scene's UI with the name "text_dialogue". That
+//  TextMeshPro object needs to have the Displayer script attached as well
 public class SimpleDialogueDisplayObject : MonoBehaviour {
 
-	public string textToShow;
-	bool useCharByCharDisplay = true;
+    public string textToShow;
+    public bool useCharByCharDisplay = false;
 
-	SimpleDialogueDisplayer displayer;
-
-
-
-	void Start () {
-		displayer = GameObject.Find ("text_dialogue").GetComponent <SimpleDialogueDisplayer> ();
-	}
-
-	void OnTriggerEnter(Collider other) {
-		if (other.CompareTag ("Player")) {
-
-			if (useCharByCharDisplay) {
-				displayer.startDialogue (textToShow);
-			} else {
-				displayer.textToDisplay = textToShow;
-			}
-
-		}
-	}
+    SimpleDialogueDisplayer displayer;
 
 
-	void OnTriggerExit(Collider other) {
-		if (other.CompareTag ("Player")) {
-			if (useCharByCharDisplay) {
-				displayer.stopDialogue ();	
-			} else {
-				displayer.textToDisplay = "";
-			}
-		}
-	}
+
+    void Start () {
+        displayer = GameObject.Find ("text_dialogue").GetComponent <SimpleDialogueDisplayer> ();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag ("Player")) {
+
+            if (useCharByCharDisplay) {
+                displayer.setMode(true);
+                displayer.startDialogue (textToShow);
+            } else {
+                displayer.setMode(false);
+                displayer.textToDisplay = textToShow;
+            }
+
+        }
+    }
+
+
+    void OnTriggerExit(Collider other) {
+        if (other.CompareTag ("Player")) {
+            if (useCharByCharDisplay) {
+                displayer.stopDialogue ();  
+            } else {
+                displayer.textToDisplay = "";
+            }
+        }
+    }
 }
--- Week 6/SimpleDialogueDisplayer.cs
+++ Week 6/SimpleDialogueDisplayer.cs
@@ -3,78 +3,103 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class SimpleDialogueDisplayer : MonoBehaviour {
+// This is attached to a textmeshpro UI text which is named 'text_dialogue'
+// You do not need to edit this script's textToDisplay property - the DisplayObject script does that
+public class SimpleDialogueDisplayer : MonoBehaviour
+{
 
-	public string textToDisplay = "";
-	TMP_Text uiTextElement;
-	// Use this for initialization
-	void Start () {
-		uiTextElement = GetComponent<TMP_Text> ();
-	}
-
-
-	// Debug stuff (change these to change behavior of script)
-	bool useSimpleDisplay = false;
-	bool useCharByCharDisplay = true;
-
-	// You can use timers to make something only happen every so often
-	float timerShowChar = 0;
-	float timerShowCharMAX = 0.03f;
-
-	int characterDisplayState = 0;
-
-	int mostRecentDisplayedCharacterIndex = 0;
+    public string textToDisplay = "";
+    TMP_Text uiTextElement;
+    // Use this for initialization
+    void Start()
+    {
+        uiTextElement = GetComponent<TMP_Text>();
+    }
 
 
+    // Debug stuff (change these to change behavior of script)
+    bool useSimpleDisplay = true;
+    bool useCharByCharDisplay = false;
 
-	// Update is called once per frame
-	void Update () {
-		if (useSimpleDisplay) {
-			uiTextElement.text = textToDisplay;
-		} else if (useCharByCharDisplay) {
-			// This coding pattern is called 'finite state machine'
+    // You can use timers to make something only happen every so often
+    float timerShowChar = 0;
+    float timerShowCharMAX = 0.03f;
 
-			// In this state, the script waits until a SimpleDialogueDisplayObject script
-			// calls the "startDialogue" function, which will transition the state to 1.
-			if (characterDisplayState == 0) {
-				uiTextElement.text = "";
+    int characterDisplayState = 0;
 
-			// In this state, the displayed text will appear character by character.
-			} else if (characterDisplayState == 1) {
-
-				timerShowChar += Time.deltaTime;
-
-				if (timerShowChar > timerShowCharMAX) {
-					timerShowChar = 0;
-					// e.g., if textToDisplay is "Apple", then textToDisplay[0] is "A", etc.
+    int mostRecentDisplayedCharacterIndex = 0;
 
 
-					if (mostRecentDisplayedCharacterIndex < textToDisplay.Length) {
+    public void setMode(bool charbychar = false) {
+        if (charbychar){
+            useSimpleDisplay = false;
+            useCharByCharDisplay = true;
+        } else {
+            useSimpleDisplay = true;
+            useCharByCharDisplay = false;
+        }
+    }
 
-						if (textToDisplay[mostRecentDisplayedCharacterIndex].ToString () == ".") {
-							timerShowChar = -0.4f;
-						}
+    // Update is called once per frame
+    void Update()
+    {
+        if (useSimpleDisplay)
+        {
+            uiTextElement.text = textToDisplay;
+        }
+        else if (useCharByCharDisplay)
+        {
+            // This coding pattern is called 'finite state machine'
 
-						uiTextElement.text += textToDisplay [mostRecentDisplayedCharacterIndex];
-						mostRecentDisplayedCharacterIndex += 1;
-					}
-				}
+            // In this state, the script waits until a SimpleDialogueDisplayObject script
+            // calls the "startDialogue" function, which will transition the state to 1.
+            if (characterDisplayState == 0)
+            {
+                uiTextElement.text = "";
 
-			}
+                // In this state, the displayed text will appear character by character.
+            }
+            else if (characterDisplayState == 1)
+            {
 
-		}
-	}
+                timerShowChar += Time.deltaTime;
 
-	public void startDialogue(string _textToDisplay) {
-		characterDisplayState = 1; // Change the state to zero so text starts to display.
-		textToDisplay = _textToDisplay;
-		mostRecentDisplayedCharacterIndex = 0;
-	}
+                if (timerShowChar > timerShowCharMAX)
+                {
+                    timerShowChar = 0;
+                    // e.g., if textToDisplay is "Apple", then textToDisplay[0] is "A", etc.
 
-	// This function will be called when the player leaves the trigger of a SimpleDialogueDisplayObject.
-	public void stopDialogue() {
-		characterDisplayState = 0;
-		textToDisplay = "";
-	}
+
+                    if (mostRecentDisplayedCharacterIndex < textToDisplay.Length)
+                    {
+
+                        if (textToDisplay[mostRecentDisplayedCharacterIndex].ToString() == ".")
+                        {
+                            timerShowChar = -0.4f;
+                        }
+
+                        uiTextElement.text += textToDisplay[mostRecentDisplayedCharacterIndex];
+                        mostRecentDisplayedCharacterIndex += 1;
+                    }
+                }
+
+            }
+
+        }
+    }
+
+    public void startDialogue(string _textToDisplay)
+    {
+        characterDisplayState = 1; // Change the state to zero so text starts to display.
+        textToDisplay = _textToDisplay;
+        mostRecentDisplayedCharacterIndex = 0;
+    }
+
+    // This function will be called when the player leaves the trigger of a SimpleDialogueDisplayObject.
+    public void stopDialogue()
+    {
+        characterDisplayState = 0;
+        textToDisplay = "";
+    }
 
 }
