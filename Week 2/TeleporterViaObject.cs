7e0f2163f2820b67432875d0364752b687a3f418
Sean Han Tani <seagaia@f-m.fm>
2019-02-04 14:01:01 -0600

    update

--- Week 2/TeleporterViaObject.cs
+++ Week 2/TeleporterViaObject.cs
@@ -9,16 +9,8 @@ public class TeleporterViaObject : MonoBehaviour {
 
 	// OnTriggerStay2D, OnTriggerExit2D
 	void OnTriggerEnter2D(Collider2D playerCollider) {
-        
 		Vector3 newPlayerPosition = new Vector3 ();
 		newPlayerPosition = destinationTeleporter.transform.position;
-
-		BoxCollider2D destinationTeleportCollider = destinationTeleporter.GetComponent <BoxCollider2D> ();
-
-		newPlayerPosition.x += destinationTeleportCollider.bounds.extents.x;
-		newPlayerPosition.x += playerCollider.bounds.extents.x;
-		newPlayerPosition.x += 1f;
-		 
 		playerCollider.transform.position = newPlayerPosition;
 	}
 
