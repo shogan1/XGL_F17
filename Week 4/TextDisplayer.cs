d74ba1ce58845c5c7a37b8770b4eb4f7743ab8f6
Sean Han Tani <seagaia@f-m.fm>
2019-02-18 13:24:59 -0600

    hjytj

---
+++ Week 4/TextDisplayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextDisplayer : MonoBehaviour
+{
+    // This script expects there to be a UI Image and TMP Text object in your UI
+
+    public string TMP_Text_Object_Name = "Dialogue";
+    public string DialogueBoxName = "DialogueBox";
+
+    [TextArea(3,10)]
+    public string textToDisplay = "Add the text you want to show here. (Edit in the Inspector)";
+
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            GameObject.Find(DialogueBoxName).GetComponent<UnityEngine.UI.Image>().enabled = true;
+            GameObject.Find(TMP_Text_Object_Name).GetComponent<TMP_Text>().text = textToDisplay;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            GameObject.Find(DialogueBoxName).GetComponent<UnityEngine.UI.Image>().enabled = false;
+            // "" is also called 'the empty string'
+            GameObject.Find(TMP_Text_Object_Name).GetComponent<TMP_Text>().text = "";
+        }
+
+    }
+
+}
